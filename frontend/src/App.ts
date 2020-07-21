fcc59906b9c1d290858f3c8dc2f43551237b2662
Gabriel Almir <avlye@outlook.com>
2020-07-21 20:23:29 -0300

    Learn separation of concerns & Add Render Screen

--- frontend/src/App.ts
+++ frontend/src/App.ts
@@ -1,15 +1,58 @@
 // Loads global stylesheet
-import "./scss/main.scss"
+import './scss/main.scss';
 
-const screen = <HTMLCanvasElement> document.getElementById('screen')
-const context = screen.getContext('2d')
+const screen = <HTMLCanvasElement> document.getElementById('screen');
+const context = screen.getContext('2d');
 
-const color = 'red'
-const positionX = 0
-const positionY = 0
+interface Game {
+  players: {
+    [key: string]: {
+      x: number,
+      y: number
+    }
+  },
 
-const width = 250
-const height = 250
+  fruits: {
+    [key: string]: {
+      x: number,
+      y: number
+    }
+  }
+}
 
-context.fillStyle = color
-context.fillRect(positionX, positionY, width, height)
+const game: Game = {
+  players: {
+    player1: { x: 1, y: 1 },
+    player2: { x: 9, y: 9 },
+  },
+  fruits: {
+    fruit1: { x: 3, y: 1 },
+  },
+};
+
+function clearScreen() {
+  context.fillStyle = 'white'
+  context.fillRect(0, 0, 10, 10)
+}
+
+function renderScreen() {
+  clearScreen()
+
+  for (const playerID in game.players) {
+    const player = game.players[playerID]
+
+    context.fillStyle = 'black'
+    context.fillRect(player.x, player.y, 1, 1)
+  }
+
+  for (const fruitID in game.fruits) {
+    const fruit = game.fruits[fruitID]
+
+    context.fillStyle = 'green'
+    context.fillRect(fruit.x, fruit.y, 1, 1)
+  }
+
+  requestAnimationFrame(renderScreen)
+}
+
+renderScreen()
